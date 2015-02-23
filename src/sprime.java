e5d9736f401762dfaa93fc9ee10a0287b9c793eb
supersteph <stephen.kj.wu@gmail.com>
2015-02-22 21:18:36 -0800

    initial full program

--- src/sprime.java
+++ src/sprime.java
@@ -18,19 +18,55 @@ import java.io.*;
 import java.util.*;
 
 public class sprime {
-    public static boolean isPri(int[] x, int index){
-        int y = 0;
-        for(int i =;i++){
-            y+=x[index-i]*Math.pow(10,index-i);
-        }
-        System.out.println(y);
-        for (int i = 3;i<=Math.sqrt(y);i+=2){
+    public static boolean isPri(int x){
+        for (int i = 3;i<=Math.sqrt(x)+1;i+=2) {
+            if (x % i == 0) {
+
+                return false;
+            }
 
         }
-        return false;
+        return true;
+    }
+    public static int getCount(int x){
+        int m = 0;
+        int k = x;
+        while(k!=0){
+            m++;
+            k/=10;
+        }
+        return m;
+    }
+    public static void superpri(ArrayList<Integer> explore, int length){
+        int m = length;
+        while(getCount(explore.get(0))!=m){
+            for(int i = 3;i<=9;i+=2){
+                int k = explore.get(0)*10+i;
+                if(isPri(k)){
+                    explore.add(k);
+                }
+            }
+            explore.remove(0);
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        isPri(new int[] {1,2,3},1);
+
+        BufferedReader f = new BufferedReader(new FileReader("sprime.in"));
+        // input file name goes above
+
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("sprime.out")));
+        int n = Integer.parseInt(f.readLine());
+        ArrayList<Integer> stuff = new ArrayList<Integer>();
+        stuff.add(2);
+        stuff.add(3);
+        stuff.add(5);
+        stuff.add(7);
+        superpri(stuff,n);
+        for(Integer i: stuff){
+            out.println(i);
+        }
+        out.close();
+        System.exit(0);
     }
 }
