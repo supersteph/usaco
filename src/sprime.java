0ae9f2b7b92619243ee3ee16a19ef004bfccbaed
supersteph <stephen.kj.wu@gmail.com>
2015-02-22 19:27:06 -0800

    edit high level solution

--- src/sprime.java
+++ src/sprime.java
@@ -6,10 +6,9 @@ TASK: sprime
 
 /**
  * How to solve it:
- *
- * I have two  a to explore method, and while the last element in the array is not = 0 then
- * i keep on going and add an odd number to the end to check to see if it is going to keep
- * on being a prime number
+ *So i get the first prime number, and then i add the second number to it and check to make sure that it is prime
+ * if it is prime then i add it to the end of an arraylist of things to be explored if it isn't then i don't, when
+ * i get to the first thing that shows that the entire thing is a super prime then i return
  *
  *
  */
@@ -17,19 +16,21 @@ TASK: sprime
 
 import java.io.*;
 import java.util.*;
+
 public class sprime {
-    public static boolean isPri(int[] x, index){
+    public static boolean isPri(int[] x, int index){
         int y = 0;
-        for(int i = 0; i<= index;i++){
-            y+=x[i]*Math.pow(10,index-i);
+        for(int i =;i++){
+            y+=x[index-i]*Math.pow(10,index-i);
         }
         System.out.println(y);
+        for (int i = 3;i<=Math.sqrt(y);i+=2){
+
+        }
         return false;
     }
 
     public static void main(String[] args) throws IOException {
-        isPri(new {1,2,3},1);
-
-
+        isPri(new int[] {1,2,3},1);
     }
 }
