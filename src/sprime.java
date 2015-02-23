9206b32d216dac100433cc4b758bbe59341338b0
supersteph <stephen.kj.wu@gmail.com>
2015-02-22 18:28:32 -0800

    add high level solution

--- src/sprime.java
+++ src/sprime.java
@@ -4,53 +4,32 @@ LANG: JAVA
 TASK: sprime
 */
 
+/**
+ * How to solve it:
+ *
+ * I have two  a to explore method, and while the last element in the array is not = 0 then
+ * i keep on going and add an odd number to the end to check to see if it is going to keep
+ * on being a prime number
+ *
+ *
+ */
+
+
 import java.io.*;
 import java.util.*;
 public class sprime {
+    public static boolean isPri(int[] x, index){
+        int y = 0;
+        for(int i = 0; i<= index;i++){
+            y+=x[i]*Math.pow(10,index-i);
+        }
+        System.out.println(y);
+        return false;
+    }
 
     public static void main(String[] args) throws IOException {
-	// write your code here
-        BufferedReader f = new BufferedReader(new FileReader("ride.in"));
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
-        String group = f.readLine();
-        int gc= group.length();
-        String comet = f.readLine();
-        int cc = comet.length();
-        int gn = 0;
-        int gtotal = 1;
-        char a = 'A';
-        for(int i=0;i<gc;i++)
-        {
+        isPri(new {1,2,3},1);
 
-            char x = Character.toLowerCase(group.charAt(i));
-
-
-
-            gtotal *= (int)x-(int)'a'+1;
-
-
-        }
-
-        int gnum= gtotal%47;
-        int cn = 0;
-
-        int ctotal = 1;
-        for(int i=0;i<cc;i++)
-        {
-            char x = Character.toLowerCase(comet.charAt(i));
-            ctotal *= (int)x-'a'+1;
-
-
-        }
-        int cnum= ctotal%47;
-        if(cnum==gnum) {
-            out.println("GO");
-
-        } else {
-            out.println("STAY");
-        }
-        out.close();
-        System.exit(0);
 
     }
 }
