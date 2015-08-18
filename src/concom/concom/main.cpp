4d5b27d71244ce720e3fe20136710efeb05d5248
Wei Liu <steph@Weis-MacBook-Pro.local>
2015-08-17 21:46:50 -0700

    doing controlling

--- src/concom/concom/main.cpp
+++ src/concom/concom/main.cpp
@@ -1,15 +1,88 @@
-//
-//  main.cpp
-//  concom
-//
-//  Created by Wei Liu on 8/16/15.
-//  Copyright (c) 2015 Wei Liu. All rights reserved.
-//
+/*
+ ID: xiaoyun4
+ PROG: concom
+ LANG: C++
+ */
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+//#include <istream>
+
+using namespace std;
+
+class company {
+    public:
+    int num;
+    vector<company*> othercomps;
+    vector<int> percent;
+    vector<int> owned;
+};
+
+vector<int> get(company* x){
+    if(x->othercomps.size()==0){
+        return *new vector<int>(0);
+    }
+    
+    for(int i = 0; i<x->othercomps.size();i++){
+        if(x->percent[i]>50){
+            
+            
+        }
+        
+        vector<int> stuff = get(x->othercomps[i]);
+        for(int i = 0; i<stuff.size();i++){
+            x->owned.push_back(stuff[i]);
+        }
+        
+    }
+    
+    return x->owned;
+    
+}
+
+void tryget(company* x){
+    if(x->othercomps.size()==0){
+        return;
+    }
+    
+    for(int i = 0; i<x->othercomps.size();i++){
+        if(x->percent[i]>50){
+            x->owned.push_back(x->othercomps[i]->num);
+            x->percent.erase(x->percent.begin()+i-1);
+            x->othercomps.erase(x->othercomps.begin()+i-1);
+        }
+        tryget(x->othercomps[i]);
+        
+    }
+    
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //cout<<"yo \n";
+    ofstream fout ("money.out");
+    //fout<< "yo";
+    //fout.close();
+    
+    ifstream fin ("money.in");
+    //fout<<"yo"<<"\n";
+    //fout.close();
+    //cout<<"yo";
+    
+    int k;
+    fin>>k;
+    for(int i = 0; i<k;i++){
+        
+    }
+    
+
+    fout.close();
+    
+    
+    
     return 0;
 }
+
